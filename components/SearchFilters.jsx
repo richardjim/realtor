45e28217d659a7cd92d132125fe9c93827905cee
Iyevhobu Richard <richardiyevhobu@gmail.com>
2022-09-20 11:48:29 +0100

    searchpropertyfilter

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -14,9 +14,29 @@ import {
 import Router from "next/router";
 import { mdCancel } from "react-icons/md";
 import Image from "next/image";
+import { getFilterValues, filterData } from "../utils/filterData";
 
 const SearchFilters = () => {
-  return <div>SearchFilter</div>;
+  const [filters, setFilters] = useState(filterData);
+
+  const searchProperties = () => {};
+
+  return (
+    <Flex bg="gray.100" justifyContent="center" flexWrap="wrap">
+      {filters.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select
+            onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
+            w="fit-content"
+            p="2"
+            placeholder={filter.placeholder}
+          ></Select>
+        </Box>
+      ))}
+    </Flex>
+  );
 };
 
 export default SearchFilters;
