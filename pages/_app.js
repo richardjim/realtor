1e4eb12febde8d2546a40856ad8163ee2c931f1f
Iyevhobu Richard <richardiyevhobu@gmail.com>
2022-09-21 09:46:32 +0100

    err update

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import Head from "next/head";
 import Router from "next/router";
+import Head from "next/head";
 import NProgress from "nprogress";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import Layout from "../components/Layout";
+
 function MyApp({ Component, pageProps }) {
   NProgress.configure({ showSpinner: false });
 
